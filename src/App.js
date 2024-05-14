92ae2395bb4004369a66d7e702c0fe2db258e839
The-EleetCoder <jaijain1803@gmail.com>
2024-05-14 18:10:30 +0530

    added realtime resizing functionality

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import { useState, useEffect } from "react";
+import Split from "react-split";
 import Editor from "./components/Editor";
 
 function App() {
-
   const [html, setHtml] = useState("");
   const [css, setCss] = useState("");
   const [js, setJs] = useState("");
-  const [srcDoc, setSrcDoc] = useState()
+  const [srcDoc, setSrcDoc] = useState();
 
   useEffect(() => {
     const timeout = setTimeout(() => {
@@ -16,45 +16,40 @@ function App() {
           <style>${css}</style>
           <script>${js}</script>
         </html>
-      `)
-    }, 250)
+      `);
+    }, 250);
 
-    return () => clearTimeout(timeout)
-  }, [html, css, js])
-
+    return () => clearTimeout(timeout);
+  }, [html, css, js]);
 
   return (
     <div className="wrapper">
-      {/* top section */}
-      <div className="pane top-pane">
-        <Editor
-          displayName="HTML"
-          value={html}
-          onChange={setHtml}
-        />
-        <Editor
-          displayName="CSS"
-          value={css}
-          onChange={setCss}
-        />
-        <Editor
-          displayName="JS"
-          value={js}
-          onChange={setJs}
-        />
-      </div>
+      <Split sizes={[50, 50]} direction="vertical" className="split-vertical">
+        {/* top half */}
+        <Split sizes={[33, 33, 34]} className="split-horizontal top-pane">
+          <div className="pane">
+            <Editor displayName="HTML" value={html} onChange={setHtml} />
+          </div>
+          <div className="pane">
+            <Editor displayName="CSS" value={css} onChange={setCss} />
+          </div>
+          <div className="pane">
+            <Editor displayName="JS" value={js} onChange={setJs} />
+          </div>
+        </Split>
 
-      {/* bottom section */}
-      <div className="pane">
-        <iframe
-          srcDoc={srcDoc}
-          className="iframe"
-          title="output"
-          sandbox="allow-scripts"
-          width="100%"
-          height="100%"
-        />
-      </div>
+        {/* bottom half */}
+        <div className="pane">
+          <iframe
+            srcDoc={srcDoc}
+            className="iframe"
+            title="output"
+            sandbox="allow-scripts"
+            width="100%"
+            height="100%"
+          />
+        </div>
+      </Split>
     </div>
   );
 }
--- src/components/Editor.js
+++ src/components/Editor.js
@@ -2,25 +2,19 @@ import React from "react";
 import CodeMirror from "@uiw/react-codemirror";
 import { javascript } from "@codemirror/lang-javascript";
 import { EditorView } from "@codemirror/view";
-import {sublime} from '@uiw/codemirror-theme-sublime'
+import { sublime } from "@uiw/codemirror-theme-sublime";
 
 const Editor = (props) => {
   const { displayName, value, onChange } = props;
 
   return (
     <div className="editor-container">
-      <div className="editor-title">
-        {displayName}
-        <button>o/c</button>
-      </div>
+      <div className="editor-title">{displayName}</div>
       <CodeMirror
         className="codemirror-editor"
         theme={sublime}
         value={value}
-        extensions={[
-          javascript({ jsx: true }), 
-          EditorView.lineWrapping
-        ]}
+        extensions={[javascript({ jsx: true }), EditorView.lineWrapping]}
         height="100%"
         onChange={onChange}
       />
